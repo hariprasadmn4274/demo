f4a7e17ce0bd15bc081ad49dc7447caf039bce44
hariprasadmn <140478782+hariprasadmn@users.noreply.github.com>
2024-07-05 16:47:56 +0530

    Initial commit

---
+++ cypress/support/index.js
@@ -0,0 +1,9 @@
+
+// Import commands.js using ES2015 syntax:
+import './commands'
+
+// Alternatively you can use CommonJS syntax:
+// require('./commands')
+
+// Add any additional setup or plugins here
+require('cypress-xpath');
